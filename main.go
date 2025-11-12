17cdcf898ac8a252347ef57fb27201570b25d3fc
Eniola Omotee <40490137+eniolaomotee@users.noreply.github.com>
2025-11-12 17:05:45 +0100

    feat:REPL commands

--- main.go
+++ main.go
@@ -1,10 +1,61 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 )
 
+type cliCommand struct{
+	name string
+	description string
+	callback func() error
+}
 
 func main(){
-	fmt.Println("Hello, World!")
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		scanned := scanner.Scan()
+		if !scanned{
+			return 
+		}
+		word := scanner.Text()
+		text := strings.ToLower(word)
+		firstWord := strings.Fields(text)
+
+		cmd := firstWord[0]
+
+		if command, ok := getCommands()[cmd]; ok{
+			if err := command.callback(); err != nil{
+				fmt.Println("error", err)
+			}
+			continue
+		}
+
+		fmt.Println("Unknown command")
+		continue
+		
+	}
+
 }
+
+
+func getCommands() map[string]cliCommand{
+	return map[string]cliCommand{
+		"help":{
+			name: "help",
+			description: "Displays a help message",
+			callback: commandHelp,
+		},
+		"exit":{
+			name: "exit",
+			description: "Exit the Pokedex",
+			callback: commandExit,
+		},
+	}
+}
+
